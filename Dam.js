8521b3ee3fe570529632de30f71c7c5dabaa9dbc
Almujtabashawgi <125862283+Almujtabashawgi@users.noreply.github.com>
2025-05-28 14:21:20 +0200

    aa

---
+++ Dam.js
@@ -0,0 +1,7 @@
+var damjson = {
+"type": "FeatureCollection",
+"name": "Dam",
+"features": [
+{ "type": "Feature", "properties": { "Id": 0 }, "geometry": { "type": "LineString", "coordinates": [ [ 31.9052848, 18.4543153 ], [ 31.9052006, 18.4538373 ], [ 31.9049739, 18.4526996 ] ] } }
+]
+}
---
+++ damSite.js
@@ -0,0 +1,7 @@
+var damSitejson = {
+"type": "FeatureCollection",
+"name": "dam site",
+"features": [
+{ "type": "Feature", "properties": { "Name": "Untitled Placemark", "FolderPath": "P.Point.kmz", "SymbolID": 0, "AltMode": -1, "Base": 0.0, "Snippet": null, "PopupInfo": null, "HasLabel": -1, "LabelID": 0 }, "geometry": { "type": "Point", "coordinates": [ 31.9052006, 18.4538373, 0.0 ] } }
+]
+}

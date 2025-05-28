f3df8554c9710e639ce360c2eb45b7405f7a3328
Almujtabashawgi <125862283+Almujtabashawgi@users.noreply.github.com>
2025-05-28 14:36:08 +0200

    aa

---
+++ spillway.js
@@ -0,0 +1,5 @@
+var spillwayjson = {"type": "FeatureCollection","name": "spillway","features": [
+{ "type": "Feature", "properties": { "arcid": 1, "grid_code": 1, "from_node": 3, "to_node": -2, "Shape_Leng": 6.2500000018600002 }, "geometry": { "type": "LineString", "coordinates": [ [ 31.9053404, 18.4548804 ], [ 31.9053996, 18.4548807 ] ] } },
+{ "type": "Feature", "properties": { "arcid": 2, "grid_code": 3, "from_node": -1, "to_node": 3, "Shape_Leng": 124.66043683300001 }, "geometry": { "type": "LineString", "coordinates": [ [ 31.904216, 18.4548739 ], [ 31.9043351, 18.4547616 ], [ 31.9053404, 18.4548804 ] ] } }
+]
+}
